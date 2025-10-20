dbb15ba5b504489bd83a78fb94807bee60a7f5a8
Elias Vasquez Roca <eliasvroca@gmail.com>
2025-10-20 14:20:18 -0400

    add: ruta base admin

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::prefix('admin')->name('admin.')->group(fn() => require __DIR__.'/admin.php');
 });
