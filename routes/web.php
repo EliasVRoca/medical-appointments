4181cd9e72166b3edbf709dd5f14d86b1cf94aef
Elias Vasquez Roca <eliasvroca@gmail.com>
2025-10-20 14:34:52 -0400

    change: anulando ruta / y ruta /dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/admin');
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
     Route::prefix('admin')->name('admin.')->group(fn() => require __DIR__.'/admin.php');
 });
