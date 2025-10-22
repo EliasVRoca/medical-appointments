61cdcb1c1defa963181fa8b5de6f9329d59e9d3d
Elias Vasquez Roca <eliasvroca@gmail.com>
2025-10-22 11:36:43 -0400

    add: plantilla de administrador

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+  Hola como estas desde el dashboard
+</x-admin-layout>
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/admin');
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
-    // Route::get('/dashboard', function () {
-    //     return view('dashboard');
-    // })->name('dashboard');
+    Route::get('/dashboard', function () {
+        return redirect('/admin');
+    })->name('dashboard');
     Route::prefix('admin')->name('admin.')->group(fn() => require __DIR__.'/admin.php');
 });
